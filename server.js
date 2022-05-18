3476c664a26617a14cdc92fbe1a9b466de96145c
Jiho Kim <jihok0519@gmail.com>
2022-05-18 16:51:34 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const budget = require('./models/budget');
+const app = express();
+const port = 3000;
+
+app.get("/budgets", (req, res) => {
+    res.render("index.ejs", {
+        allBudgets: budget,
+    });
+});
+
+app.get("/budgets/new", (req, res) => {
+    res.render("new.ejs");
+})
+
+app.post("/budgets", (req, res, next) => {
+
+});
+
+app.get("/budgets/:index", (req, res) => {
+    res.render("show.ejs", {
+        budgets: budget[req.params.index],
+    });
+});
+
+app.listen(port, () => {
+    console.log("Listening on port", port);
+})
